73c24f8d034e7554309b2c2dd29918b2de4bb36c
miresque <mirkilinc@gmail.com>
2022-07-19 18:03:16 +0100

    extension 1 complete

--- src/component/Store.js
+++ src/component/Store.js
@@ -2,14 +2,17 @@ import '../styles/index.css'
 
 import initialStoreItems from '../store-items'
 import StoreItem from '../component/StoreItem'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Cart from './Cart'
 
 console.log(initialStoreItems)
 
 export default function Store() {
   const [storeItems, setStoreItems] = useState(initialStoreItems)
+  const [storeItemsOnDisplay, setStoreItemsOnDisplay] = useState(storeItems)
   const [cartItems, setCartItems] = useState([])
+  const [selectedType, setSelectedType] = useState('none')
+  const [sortedStatus, setSortedStatus] = useState(false)
 
   const addToCart = (item) => {
     let newCart = [...cartItems]
@@ -32,12 +35,63 @@ export default function Store() {
     setCartItems(newCart)
   }
 
+  const filterByType = () => {
+    let filteredStoreItems = [...storeItems]
+    if (selectedType === 'Veg') {
+        filteredStoreItems = storeItems.filter(item => item.type === 'Veg')
+    } else if(selectedType === 'Fruit') {
+        filteredStoreItems = storeItems.filter(item => item.type === 'Fruit')
+    }
+    setStoreItemsOnDisplay(filteredStoreItems)
+  }
+
+  const sortByPrice = () => {
+    let sortedStoreItems = [...storeItemsOnDisplay]
+    if (sortedStatus) {
+        setSortedStatus(!sortedStatus)
+        sortedStoreItems.sort((a, b) => {
+            return a.price - b.price
+        })
+        console.log(sortedStoreItems)
+    } else if(!sortedStatus) {
+        setSortedStatus(!sortedStatus)
+        sortedStoreItems.sort((a, b) => {
+            return b.price - a.price
+        })
+        console.log(sortedStoreItems)
+    }
+    setStoreItemsOnDisplay(sortedStoreItems)
+  }
+
+  useEffect(() => {
+        filterByType()
+    },
+    [selectedType]
+  )
+
   return (
     <>
       <header id="store">
         <h1>Greengrocers</h1>
+        <ul className="filter-sort">
+            <li>
+                <form id='filter-by-type-form'>
+                    <label>
+                        <p>Filter By Type:</p>
+                        <select value={selectedType} onChange={(e) => setSelectedType(e.target.value)}>
+                            <option value="none"></option>
+                            <option value="Veg">Veg</option>
+                            <option value="Fruit">Fruit</option>
+                        </select>
+                    </label>
+                </form>
+            </li>
+            <li>
+                <button onClick={sortByPrice}>Sort By Price</button>
+            </li>
+        </ul>
         <ul className="item-list store--item-list">
-          {storeItems.map(item => (
+          {storeItemsOnDisplay.map(item => (
               <StoreItem key={item.id} item={item} add={addToCart} />
           ))}
         </ul>
--- src/store-items.js
+++ src/store-items.js
@@ -3,52 +3,62 @@ const storeItems =  [
   {
     id: "001-beetroot",
     name: "beetroot",
-    price: 0.35
+    price: 0.35,
+    type: "Veg"
   },
   {
     id: "002-carrot",
     name: "carrot",
-    price: 0.35
+    price: 0.25,
+    type: "Veg"
   },
   {
     id: "003-apple",
     name: "apple",
-    price: 0.35
+    price: 0.25,
+    type: "Fruit"
   },
   {
     id: "004-apricot",
     name: "apricot",
-    price: 0.35
+    price: 0.55,
+    type: "Fruit"
   },
   {
     id: "005-avocado",
     name: "avocado",
-    price: 0.35
+    price: 1,
+    type: "Fruit"
   },
   {
     id: "006-bananas",
     name: "bananas",
-    price: 0.35
+    price: 0.35,
+    type: "Fruit"
   },
   {
     id: "007-bell-pepper",
     name: "bell pepper",
-    price: 0.35
+    price: 0.35,
+    type: "Veg"
   },
   {
     id: "008-berry",
     name: "berry",
-    price: 0.35
+    price: 0.55,
+    type: "Fruit"
   },
   {
     id: "009-blueberry",
     name: "blueberry",
-    price: 0.35
+    price: 0.55,
+    type: "Fruit"
   },
   {
     id: "010-eggplant",
     name: "eggplant",
-    price: 0.35
+    price: 0.65,
+    type: "Fruit"
   }
 ]
 
